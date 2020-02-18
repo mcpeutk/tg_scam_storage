0d56d33c7709e32a26720e750cb3c323f951e2d8
mcpeutk <mcpeutk@gmail.com>
2020-02-18 14:02:57 +0300

    User creation and check in db added

--- controller.py
+++ controller.py
@@ -3,7 +3,8 @@ class Controller:
         self._users_management = users_management
 
     async def start_bot(self, chat_id):
-        pass
+        if (await self._users_management.get_user_by_id(chat_id) == None):
+            await self._users_management.create_user(chat_id)
 
     async def proceed_unknown_message(self, chat_id, message):
         pass
--- db_manage.py
+++ db_manage.py
@@ -1,8 +1,12 @@
 import sqlite3
+import aiosqlite
+import datetime
 
 class UsersManagement:
+    db_name = "bot.db"
+
     def __init__(self):
-        conn = sqlite3.connect("bot.db")
+        conn = sqlite3.connect(self.db_name)
         c = conn.cursor()
         c.execute("CREATE TABLE IF NOT EXISTS users (chat_id text, \
                                                      start_date text, \
@@ -11,3 +15,39 @@ class UsersManagement:
         conn.commit()
         conn.close()
 
+    async def get_user_by_id(self, chat_id):
+        conn = await aiosqlite.connect(self.db_name)
+
+        query = '''SELECT * FROM users WHERE chat_id = ?'''
+        data = (chat_id,)
+        c = await conn.execute(query, data)
+
+        result = await c.fetchall()
+
+        await c.close()
+        await conn.close()
+
+        if (len(result) == 0):
+            return None
+        return result[0]
+
+    async def create_user(self, chat_id):
+        curr_time = datetime.datetime.now().strftime("%d-%m-%Y %H:%M")
+
+        conn = await aiosqlite.connect(self.db_name)
+
+        query = '''INSERT INTO users VALUES (?, ?, ?, ?)'''
+        user_data = (chat_id, curr_time, curr_time, None)
+
+        await conn.execute(query, user_data)
+        await conn.commit()
+        await conn.close()
+
+# if __name__ == "__main__":
+#     import asyncio
+
+#     users = UsersManagement()
+
+#     asyncio.run(users.create_user(2))
+
+#     print(asyncio.run(users.get_user_by_id(2)))
